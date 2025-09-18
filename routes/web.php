19f0fcd69c4856eb15a199cedc020323449cf6fb
YorguiSanchez <ysanchez192@unab.edu.co>
2025-09-18 12:15:55 -0500

    Grupo de rutas basico

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/create', [ProductController::class, 'create']);
-Route::get('products/{id}/{category?}', [ProductController::class, 'show']);
+Route::profix('products')->controller(ProductController::class)->group(function () {
+   Route::get('/', 'index');
+Route::get('/create', 'create');
+Route::get('/{id}/{category?}', 'show');
+});
