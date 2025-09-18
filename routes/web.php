cdcac94838594af6d6d73197d57ae9f67aac4e33
YorguiSanchez <ysanchez192@unab.edu.co>
2025-09-18 12:01:08 -0500

    Controlador Basico

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', function (){
-    return "LISTADO DE PRODUCTOS";
-});
-
-Route::get('/products/create}', function (){
-    return "FORMULARIO DE CREACION DE PRODUCTOS:";
-});
-
-Route::get('products/{id}/{category?}', function($id, $category = null){
-    if ($category == null){
-        return "Detalle de cada producto: " . $id ;
-    }else{
-        return "Detalle de cada producto: " . $id . " de la categoria: " . $category;
-    }
-});
-
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::get('products/{id}/{category?}', [ProductController::class, 'show']);
