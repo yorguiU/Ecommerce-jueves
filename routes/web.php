82722044b765fc0e6ad7ac7b36cabad0b39e9bca
YorguiSanchez <ysanchez192@unab.edu.co>
2025-09-18 11:29:26 -0500

    Rutas Basico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function (){
+    return "LISTADO DE PRODUCTOS";
+});
+
+Route::get('/products/create}', function (){
+    return "FORMULARIO DE CREACION DE PRODUCTOS:";
+});
+
+Route::get('products/{id}/{category}', function($id, $category){
+    return "Detalle de cada producto: " . $id . " de la categoria: " . $category;
+});
