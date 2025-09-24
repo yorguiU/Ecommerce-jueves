7548d89473bfef6e55f601ac4bbdd9b2c898da0c
yorgui sanchez <yorguisanchez3@gmail.com>
2025-09-24 16:37:24 -0500

    Vistas basicas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
     }
 
     function create(){
-         return "FORMULARIO DE CREACION DE PRODUCTOS:";
+         view('products.create');
     }
 
     function show ($id, $category = null){
